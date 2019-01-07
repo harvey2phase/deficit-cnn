a26ce36b681bf04c702aff055e8e499784cd83cb
Harvey Wang <harveywang@Harveys-MacBook-Pro-2.local>
2019-01-07 07:46:01 -0400

    not sure what happped

--- plot.py
+++ plot.py
@@ -1,9 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-file2 = np.loadtxt("Code/Data/RawDeficitsID1Gammap7.5Gamman6.5N10000Number1000R3.0nd2Alpha2.27AvgDeg4ANDSingleSingleSeed1ScaleFreepA0.0")
+death_age = 'Code/Data/RawDeathAgeDataID1Gammap7.5Gamman6.5N10000Number1000R3.0nd2Alpha2.27AvgDeg4ANDSingleSingleSeed1ScaleFreepA0.0'
 
-for f in file2[0]:
-    print(f)
-plt.plot(file2[0])
-plt.savefig('deficit column 0', dpi = 1000)
+#network_degrees = 'Code/Data/NetworkDegreesGammap7.5Gamman6.5N10000Number1000R3.0nd2Alpha2.27AvgDeg4ANDSingleSingleSeed1ScaleFreepA0.0'
+
+plt.plot(death_age)
+plt.show()
+#plt.savefig('deficits col0 vs col1', dpi = 1000)
