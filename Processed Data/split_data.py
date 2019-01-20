3ffe8de29bb3f7f19c5d6d5cfb4e7342332fa45a
Harvey Wang <harveywang@Harveys-MacBook-Pro-2.local>
2019-01-19 22:52:46 -0400

    finished splitting data 👌

---
+++ Processed Data/split_data.py
@@ -0,0 +1,44 @@
+import numpy as np
+import os
+
+def create_textfile(filename):
+    filename = "individual_data/" + str(int(filename))
+    try:
+        f = open(filename + ".txt", "x")
+
+    except FileExistsError:
+        count = 2
+        while os.path.isfile(filename + "(" + str(count) + ").txt"):
+            count += 1
+        f = open(filename + "(" + str(count) + ").txt", "x")
+
+    return f
+
+gamma_0 = 0.00183       # (unit: per year) divide to get year.
+
+death_year = np.loadtxt("death_year.txt")
+raw_deficits = np.loadtxt("raw_deficits.0")
+
+# The three columns in file "raw_deficits"
+change_time = raw_deficits[:, 0] / gamma_0
+node_id = raw_deficits[:, 1]
+state = raw_deficits[:, 2]
+
+raw_length = len(change_time) - 1
+
+
+year = 0
+f = create_textfile(death_year[year])
+
+for change in range(0, raw_length - 1):
+    f.write(str(change_time[change]) + "\t" + str(int(node_id[change])) + "\t")
+    f.write(str(state[change]) + "\n")
+    if change_time[change + 1] - change_time[change] < 0:
+        year += 1
+        f.close()
+        f = create_textfile(death_year[year])
+
+f.write(str(change_time[raw_length]) + "\t")
+f.write(str(int(node_id[raw_length])))
+f.write("\t" + str(state[raw_length]) + "\n")
+f.close()
