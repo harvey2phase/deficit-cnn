56ce1267af25f228c111466d2fcbe6b3f2e4cdb2
Harvey Wang <harvey@dal.ca>
2019-04-30 16:54:21 -0300

    trying to compile...

--- Spencer's Code/run.sh
+++ Spencer's Code/run.sh
@@ -1,14 +1,14 @@
-number=1000 # number of runs
-R=3.0 #Gamma+/Gamma-
-gammap=7.5 #damage rate constant
-gamman=6.5 #repair rate constant
-N=10000 #network size
-FIn=32 #number of frailty nodes 
-nd=2 #number of mortality nodes
-alpha=2.27 #scale free exponent
-avgdeg=4 #average degree(must be even for scalefree and smallworld)
-Mortality="AND" #mortality condition
-Topology="Single" #"Single" #network topology, single creates one network for all runs, otherwise creates a new network for each run with a different seed 
+number=1000         # number of runs
+R=3.0               # Gamma+/Gamma-
+gammap=7.5          # damage rate constant
+gamman=6.5          # repair rate constant
+N=10000             # network size
+FIn=32              # number of frailty nodes
+nd=2                # number of mortality nodes
+alpha=2.27          # scale free exponent
+avgdeg=4            # average degree(must be even for scalefree and smallworld)
+Mortality="AND"     # mortality condition
+Topology="Single"   #"Single" #network topology, single creates one network for all runs, otherwise creates a new network for each run with a different seed
 Folder="Local"
 NetworkType="ScaleFree"
 SingleSeed=1
@@ -16,7 +16,7 @@ pA=0.0
 make
 
 for k in `seq -f "%.0f" 1 1`
-do	
+do
     ./main $number $R $gammap $gamman $N $nd $alpha $avgdeg $Mortality $Topology $Folder $NetworkType $k $SingleSeed $pA
 done
 
