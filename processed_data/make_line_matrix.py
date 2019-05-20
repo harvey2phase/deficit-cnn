77485eef42ce18b84b8c5170d1eae1a33752894a
Harvey Wang <harvey@dal.ca>
2019-05-20 11:44:23 -0300

    added parameter for tranform function

--- processed_data/make_line_matrix.py
+++ processed_data/make_line_matrix.py
@@ -28,23 +28,6 @@ MATRIX_LENGTH_RANGE = range(50)
 # Functions
 #-------------------------------------------------------------------------------
 
-def dot_to_line_matrix(input_folder, filename):
-    matrix = np.loadtxt(input_folder + filename)
-    length_range = range(len(matrix))
-    width_range = range(len(matrix[0]))     #range(0, 120)
-    for w in MATRIX_WIDTH_RANGE:
-        if w > int(filename[0] + filename[1], 10):
-            for l in MATRIX_LENGTH_RANGE:
-                matrix[l][w] = -2
-            break
-        for l in MATRIX_LENGTH_RANGE:
-            if (matrix[l][w] == 0):
-                matrix[l][w] = matrix[l][w-1]
-            elif (matrix[l][w] == -1):
-                matrix[l][w] = 0
-
-    return matrix
-
 # This function takes in an output 2D matrix, then writes the matrix to the
 # file (creates the file if it does not exist).
 # Parameters:
@@ -57,8 +40,6 @@ def write_to_file(output_dir, filename, matrix):
         f.write(" ".join(map(str, m)) + "\n")
     f.close()
 
-
-
 # This function transforms all the data (.txt) in one directory to an another
 # Parameters:
 #   input_folder: specifies the directory that contains subdirectories of data;
@@ -68,7 +49,11 @@ def write_to_file(output_dir, filename, matrix):
 #     the subdirectories have names "X0", where X is the decade
 #   function: specifies the transformation for the data,
 #     takes in the input directory and filename and returns the output data
-def transform_data(input_folder, output_folder, transform_function):
+def transform_data(
+        input_folder,
+        output_folder,
+        transform_function,
+        transform_param):
     input_folder += "/"
     output_folder += "/"
 
@@ -88,7 +73,10 @@ def transform_data(input_folder, output_folder, transform_function):
             # Iterate through all duplicates of the year
             duplicate = 2
             while os.path.isfile(input_dir + filename):
-                output = transform_function(input_dir, filename)
+                output = transform_function(
+                        input_dir,
+                        filename,
+                        transform_param)
                 write_to_file(output_dir, filename, output)
 
                 filename = decade + year + "(" + str(duplicate) + ").txt"
@@ -127,14 +115,56 @@ def plot_data(data_folder, plot_folder, plot_function):
                 filename = decade + year + "(" + str(duplicate) + ")"
                 duplicate += 1
 
+def dot_to_line_matrix(input_folder, filename, param):
+    matrix = np.loadtxt(input_folder + filename)
+    for w in MATRIX_WIDTH_RANGE:
+        # FIXME Does not work for decades >= 100
+        if w > int(filename[0] + filename[1], 10):
+            for l in MATRIX_LENGTH_RANGE:
+                matrix[l][w] = -2
+            break
+        for l in MATRIX_LENGTH_RANGE:
+            if (matrix[l][w] == 0):
+                matrix[l][w] = matrix[l][w-1]
+            elif (matrix[l][w] == -1):
+                matrix[l][w] = 0
+
+    return matrix
+
 # TODO
 # Truncate to line images to X year-period health state Y years before death
 # Somehow differentiate data for forty year olds and 80 year olds
 # (Maybe use 4 for damaged for age 40 and 8 for eighty instead of all 1)
 
+# TODO
+# This function transforms line matrices to 5-year data of "dead in 5 years"
+# Parameters:
+#   input_folder: folder that contains line matrices
+#   filenmae: filename
+def dead_in_x_years(input_folder, filename, x):
+    line_matrix = np.loadtxt(input_folder + filename)
+    for w in MATRIX_WIDTH_RANGE:
+        # FIXME Does not work for decades >= 100
+        if w > int(filename[0] + filename[1], 10):
+            for l in MATRIX_LENGTH_RANGE:
+                matrix[l][w] = -2
+            break
+        for l in MATRIX_LENGTH_RANGE:
+            if (matrix[l][w] == 0):
+                matrix[l][w] = matrix[l][w-1]
+            elif (matrix[l][w] == -1):
+                matrix[l][w] = 0
+
+
+
 #-------------------------------------------------------------------------------
 # Scripts
 #-------------------------------------------------------------------------------
 
-transform_data("matrix_50nodes", "line_matrix_50nodes", dot_to_line_matrix)
+transform_data(
+        "matrix_50nodes",
+        "line_matrix_50nodes",
+        dot_to_line_matrix,
+        None
+)
 plot_data("line_matrix_50nodes", "line_image_50nodes", plt.imshow)
