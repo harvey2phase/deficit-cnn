ca25b7a395a3cb880700eb263ea378807a6a6b4d
Harvey Wang <harvey@dal.ca>
2019-05-29 12:51:32 -0300

    added training data and labels files

--- cnn/training_data/make_data.py
+++ cnn/training_data/make_data.py
@@ -1,11 +1,10 @@
 import numpy as np
 import os
 
-directory = "5_death_in_10_years_matrix/80"
-count = 0
 f = open("training_set_80.txt", "w")
+
+directory = "5_death_in_5_years_matrix/80"
 for filename in os.listdir(directory):
-    print(filename)
     matrix = np.loadtxt(directory + "/" +  filename)
 
     for row in matrix:
@@ -14,3 +13,16 @@ for filename in os.listdir(directory):
                 state = 1
             f.write(str(state) + " ")
     f.write("\n")
+
+directory = "5_death_in_10_years_matrix/80"
+for filename in os.listdir(directory):
+    matrix = np.loadtxt(directory + "/" +  filename)
+
+    for row in matrix:
+        for state in row:
+            if state != 0:
+                state = 1
+            f.write(str(state) + " ")
+    f.write("\n")
+
+f.close()
---
+++ cnn/training_data/make_labels.py
@@ -0,0 +1,8 @@
+f = open("training_labels_80.txt", "w")
+
+for i in range(303):
+    f.write("5\n")
+for i in range(303):
+    f.write("10\n")
+
+f.close
