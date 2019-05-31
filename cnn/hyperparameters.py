b32dbb6f1fd5340601a78756bb4e849d11ec0893
Harvey Wang <harvey@dal.ca>
2019-05-31 15:47:39 -0300

    added a list of hyperparamters

---
+++ cnn/hyperparameters.py
@@ -0,0 +1,5 @@
+learning_rate = 10 ** -3        # might prevent convergence
+batch_size = 10
+steps = 10 ** 6
+dropout_rate = 0.6              # might cause underfitting
+                                # could prevent overfitting
