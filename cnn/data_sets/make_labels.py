65e145eddab63bf9015f7c5d47d635c614533186
Harvey Wang <harvey@dal.ca>
2019-05-31 15:16:10 -0300

    added training labels

--- cnn/data_sets/make_labels.py
+++ cnn/data_sets/make_labels.py
@@ -1,8 +1,8 @@
-f = open("training_labels_80.txt", "w")
+f = open("eval_labels_80.txt", "w+")
 
-for i in range(303):
+for i in range(1000):
     f.write("0\n")
-for i in range(303):
+for i in range(1000):
     f.write("1\n")
 
 f.close
