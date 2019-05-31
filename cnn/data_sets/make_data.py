d2d95850f10e5d44e90ef27de213e44b9a9f189a
Harvey Wang <harvey@dal.ca>
2019-05-31 15:03:16 -0300

    added training sets

--- cnn/data_sets/make_data.py
+++ cnn/data_sets/make_data.py
@@ -1,28 +1,30 @@
 import numpy as np
 import os
 
-f = open("training_set_80.txt", "w")
+f = open("eval_set_80.txt", "w+")
 
-directory = "5_death_in_5_years_matrix/80"
+directory = "eval_individuals/5_death_5_years/80"
 for filename in os.listdir(directory):
-    matrix = np.loadtxt(directory + "/" +  filename)
+    if filename[0] == '8':
+        matrix = np.loadtxt(directory + "/" +  filename)
 
-    for row in matrix:
-        for state in row:
-            if state != 0:
-                state = 1
-            f.write(str(state) + " ")
-    f.write("\n")
+        for row in matrix:
+            for state in row:
+                if state != 0:
+                    state = 1
+                f.write(str(state) + " ")
+        f.write("\n")
 
-directory = "5_death_in_10_years_matrix/80"
+directory = "eval_individuals/5_death_10_years/80"
 for filename in os.listdir(directory):
-    matrix = np.loadtxt(directory + "/" +  filename)
+    if filename[0] == '8':
+        matrix = np.loadtxt(directory + "/" +  filename)
 
-    for row in matrix:
-        for state in row:
-            if state != 0:
-                state = 1
-            f.write(str(state) + " ")
-    f.write("\n")
+        for row in matrix:
+            for state in row:
+                if state != 0:
+                    state = 1
+                f.write(str(state) + " ")
+        f.write("\n")
 
 f.close()
