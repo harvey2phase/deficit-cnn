37f9f96cb547c149080eb2389ff7b645e9554241
Harvey Wang <harvey@dal.ca>
2019-06-01 14:12:52 -0300

    added e5 death years

--- data/model_data/death_raw_to_year.py
+++ data/model_data/death_raw_to_year.py
@@ -2,11 +2,15 @@
 
 import numpy as np
 
-raw_filename = "e4_raw_death_age.txt"
-year_filename = "e4_death_year.txt"
+raw_filename = "../../spencer_code/Data/"
+raw_filename += "RawDeathAgeDataID1Gammap7.5Gamman6.5N10000Number100000R3."
+raw_filename += "0nd2Alpha2.27AvgDeg4ANDSingleSingleSeed1ScaleFreepA0.0"
+
+year_filename = "e5_death_year.txt"
 gamma_0 = 0.00183       # (unit: per year) divide to get year
 
 raw = np.loadtxt(raw_filename)
+
 years = raw / gamma_0
 
 year_file = open(year_filename, "w+")
