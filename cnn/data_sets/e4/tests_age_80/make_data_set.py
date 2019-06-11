045464144c3f923499a2fe86237d9ae8063a5338
Harvey Wang <harvey@dal.ca>
2019-06-11 20:40:46 -0300

    added training set and label

--- cnn/data_sets/e4/tests_age_80/make_data_set.py
+++ cnn/data_sets/e4/tests_age_80/make_data_set.py
@@ -1,15 +1,15 @@
 import numpy as np
 import os
 
-output_file = "7_year_history/train_set.txt"
-directory1 = "eval_individuals/7_death_5_years/80"
-directory2 = "eval_individuals/7_death_10_years/80"
+output_file = "7_year_history/eval_set.txt"
+directory1 = "7_year_history/eval/death_in_5"
+directory2 = "7_year_history/eval/death_in_12"
 
 f = open(output_file, "w+")
 
 for filename in os.listdir(directory1):
     if filename[0] == '8':
-        matrix = np.loadtxt(directory + "/" +  filename)
+        matrix = np.loadtxt(directory1 + "/" +  filename)
 
         for row in matrix:
             for state in row:
@@ -20,7 +20,7 @@ for filename in os.listdir(directory1):
 
 for filename in os.listdir(directory2):
     if filename[0] == '8':
-        matrix = np.loadtxt(directory + "/" +  filename)
+        matrix = np.loadtxt(directory2 + "/" +  filename)
 
         for row in matrix:
             for state in row:
