e9f0349fb22c0dcc6e0e47302fa0febfa961ba76
Harvey Wang <harvey@dal.ca>
2019-06-11 13:51:41 -0300

    reorganised cnn data directories

--- cnn/data_sets/e4/tests_age_80/make_data_set.py
+++ cnn/data_sets/e4/tests_age_80/make_data_set.py
@@ -1,10 +1,11 @@
 import numpy as np
 import os
 
-f = open("7_eval_80.txt", "w+")
-
+output_file = "7_year_history/train_set.txt"
 directory1 = "eval_individuals/7_death_5_years/80"
 directory2 = "eval_individuals/7_death_10_years/80"
+
+f = open(output_file, "w+")
 
 for filename in os.listdir(directory1):
     if filename[0] == '8':
