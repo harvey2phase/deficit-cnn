0070ff3c18c41fce64992cd4f7dcfcafb353a703
Harvey Wang <harvey@dal.ca>
2019-06-26 18:15:23 -0300

    about to add more e5 data

--- data/x_his_before_80_death_in_y.py
+++ data/x_his_before_80_death_in_y.py
@@ -63,3 +63,31 @@ transform(
         test + subtest + xy + "_" + kind + "_labels.txt",
         x, y]
 )
+kind = "eval"
+transform(
+    test + kind + "_individuals",
+    x_years_before_80_dead_in_y_years,
+    [test + subtest + xy + "_" + kind + "_set.txt",
+        test + subtest + xy + "_" + kind + "_labels.txt",
+        x, y]
+)
+
+
+x = "6"
+
+xy = x + "x" + y + "y"
+transform(
+    test + kind + "_individuals",
+    x_years_before_80_dead_in_y_years,
+    [test + subtest + xy + "_" + kind + "_set.txt",
+        test + subtest + xy + "_" + kind + "_labels.txt",
+        x, y]
+)
+kind = "eval"
+transform(
+    test + kind + "_individuals",
+    x_years_before_80_dead_in_y_years,
+    [test + subtest + xy + "_" + kind + "_set.txt",
+        test + subtest + xy + "_" + kind + "_labels.txt",
+        x, y]
+)
