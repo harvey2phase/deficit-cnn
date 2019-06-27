3008dc0a493aceb355d7df5737277ea3902d65df
Harvey Wang <harvey@dal.ca>
2019-06-27 08:27:47 -0300

    added e4 x=10 data

--- data/x_his_before_80_death_in_y.py
+++ data/x_his_before_80_death_in_y.py
@@ -53,67 +53,7 @@ test = "../cnn/data_sets/e4/tests_age_80/"
 subtest = "prob_of_death_at_80/"
 y = "5"
 
-x = "3"
-xy = x + "x" + y + "y"
-
-kind = "train"
-transform(
-    test + kind + "_individuals",
-    x_years_before_80_dead_in_y_years,
-    [test + subtest + xy + "_" + kind + "_set.txt",
-        test + subtest + xy + "_" + kind + "_labels.txt",
-        x, y]
-)
-kind = "eval"
-transform(
-    test + kind + "_individuals",
-    x_years_before_80_dead_in_y_years,
-    [test + subtest + xy + "_" + kind + "_set.txt",
-        test + subtest + xy + "_" + kind + "_labels.txt",
-        x, y]
-)
-
-x = "7"
-xy = x + "x" + y + "y"
-
-kind = "train"
-transform(
-    test + kind + "_individuals",
-    x_years_before_80_dead_in_y_years,
-    [test + subtest + xy + "_" + kind + "_set.txt",
-        test + subtest + xy + "_" + kind + "_labels.txt",
-        x, y]
-)
-kind = "eval"
-transform(
-    test + kind + "_individuals",
-    x_years_before_80_dead_in_y_years,
-    [test + subtest + xy + "_" + kind + "_set.txt",
-        test + subtest + xy + "_" + kind + "_labels.txt",
-        x, y]
-)
-
-x = "9"
-xy = x + "x" + y + "y"
-
-kind = "train"
-transform(
-    test + kind + "_individuals",
-    x_years_before_80_dead_in_y_years,
-    [test + subtest + xy + "_" + kind + "_set.txt",
-        test + subtest + xy + "_" + kind + "_labels.txt",
-        x, y]
-)
-kind = "eval"
-transform(
-    test + kind + "_individuals",
-    x_years_before_80_dead_in_y_years,
-    [test + subtest + xy + "_" + kind + "_set.txt",
-        test + subtest + xy + "_" + kind + "_labels.txt",
-        x, y]
-)
-
-x = "20"
+x = "10"
 xy = x + "x" + y + "y"
 
 kind = "train"
