9d0d885a8ce58a5205ca22e988ada57204f5ef2e
Harvey Wang <harvey@dal.ca>
2019-07-05 11:22:42 -0300

    ready to removed ind to make unbiased trainig

---
+++ cnn/data_sets/remove_individuals.sh
@@ -0,0 +1,5 @@
+ls e4/tests_age_80/prob_of_death_at_80/unbiased_train_ind | sort -R | tail -370 | while read file; do
+
+    rm e4/tests_age_80/prob_of_death_at_80/unbiased_train_ind/$file
+
+done
--- transformations/x_his_before_80_death_in_y.py
+++ transformations/x_his_before_80_death_in_y.py
@@ -50,32 +50,11 @@ def x_years_before_80_dead_in_y_years(input_folder, filename, params):
 # Scripts
 #-------------------------------------------------------------------------------
 
-test = "../cnn/data_sets/e5/tests_age_80/"
+test = "../cnn/data_sets/e4/tests_age_80/"
 subtest = "prob_of_death_at_80/"
 y = "5"
 
 x = "20"
-xy = x + "x" + y + "y"
-
-kind = "train"
-transform(
-    test + kind + "_individuals",
-    x_years_before_80_dead_in_y_years,
-    [test + subtest + xy + "_" + kind + "_set.txt",
-        test + subtest + xy + "_" + kind + "_labels.txt",
-        x, y]
-)
-kind = "eval"
-transform(
-    test + kind + "_individuals",
-    x_years_before_80_dead_in_y_years,
-    [test + subtest + xy + "_" + kind + "_set.txt",
-        test + subtest + xy + "_" + kind + "_labels.txt",
-        x, y]
-)
-
-
-x = "15"
 xy = x + "x" + y + "y"
 
 kind = "train"
