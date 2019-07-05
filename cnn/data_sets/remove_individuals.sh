f9f475fcf386fec6d372997d03f551de5c5f9d62
Harvey Wang <harvey@dal.ca>
2019-07-05 12:42:52 -0300

    created unbiased training cohort

--- cnn/data_sets/remove_individuals.sh
+++ cnn/data_sets/remove_individuals.sh
@@ -1,5 +1,5 @@
-ls e4/tests_age_80/prob_of_death_at_80/unbiased_train_ind | sort -R | tail -370 | while read file; do
+ls e4/tests_age_80/prob_of_death_at_80/unbiased_train_ind/y5_dead | sort -R | tail -370 | while read file; do
 
-    rm e4/tests_age_80/prob_of_death_at_80/unbiased_train_ind/$file
+    rm e4/tests_age_80/prob_of_death_at_80/unbiased_train_ind/y5_dead/$file
 
 done
