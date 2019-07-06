684bee1310bd51da6a4b9ebbd2bb2b80f534493a
Harvey Wang <harvey@dal.ca>
2019-07-05 23:15:21 -0300

    can parse data

--- plot_results.py
+++ plot_results.py
@@ -20,21 +20,34 @@ false_positive = []
 for i in indices:
     if results_list[i] == "accuracy":
         i += 1
-        accuracy.append(results_list[i])
-    # TODO: NOT DONE YET, CHECK VARIABLE NAMES
+        accuracy.append(float(results_list[i]))
     elif results_list[i] == "true":
         i += 1
-        if results_list[i] == "positive":
+        if results_list[i] == "positives":
             i += 1
-            true_positive.append(results_list[i])
-        elif results_list[i] == "negative":
+            true_positive.append(int(float(results_list[i])))
+        elif results_list[i] == "negatives":
             i += 1
-            true_positive.append(results_list[i])
+            true_negative.append(int(float(results_list[i])))
     elif results_list[i] == "false":
         i += 1
-        if results_list[i] == "positive":
+        if results_list[i] == "positives":
             i += 1
-            true_positive.append(results_list[i])
-        elif results_list[i] == "negative":
+            false_positive.append(int(float(results_list[i])))
+        elif results_list[i] == "negatives":
             i += 1
-            true_positive.append(results_list[i])
+            false_negative.append(int(float(results_list[i])))
+
+accuracy = np.array(accuracy)
+true_positive = np.array(true_positive)
+false_negative = np.array(false_negative)
+true_negative = np.array(true_negative)
+false_positive = np.array(false_positive)
+
+print(accuracy)
+print(true_positive)
+print(false_negative)
+print(true_negative)
+print(false_positive)
+print(true_positive + false_negative)
+print(true_negative + false_positive)
