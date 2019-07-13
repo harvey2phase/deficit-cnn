f3f9a5a1ef46e3b58dfead6aeac332bd62c70d70
Harvey Wang <harvey@dal.ca>
2019-07-13 18:01:54 -0300

    finished make node list script

--- raw_data/nodes/make_node_list.py
+++ raw_data/nodes/make_node_list.py
@@ -1,24 +1,21 @@
 import numpy as np
 
 #-------------------------------------------------------------------------------
-# Constants
+# Constants and global variables
 #-------------------------------------------------------------------------------
 
 deg_filename = "network_degrees_degrees.txt"
 annk_filename = "network_degrees_ANNK.txt"
-deg_count = 10
-annd_count = 10
-output_filename = "60_annk_nodes.txt"
 
 COUNT = 1
 
 #-------------------------------------------------------------------------------
-# Script
+# Supporting functions
 #-------------------------------------------------------------------------------
 
 def load_net_deg(filename):
     return np.loadtxt(
-        deg_filename,
+        filename,
         dtype = {
             "names": ("id", "deg", "annk"),
             "formats": (np.int, np.int, np.float)
@@ -36,40 +33,33 @@ def print_line(line):
     COUNT += 1
 
 def write_line(output_file, line):
+    global COUNT
     output_file.write(
         str(line["id"]) + "\t" +
         str(line["deg"]) + "\t" +
-        str(line["annk"]) + "\n"
+        str(line["annk"]) + "\t" +
+        str(COUNT) + "\n"
     )
+    COUNT += 1
+
+def make_decreasing_annk_list(output_filename, deg_count, annk_count):
+    highest_deg = load_net_deg(deg_filename)[-deg_count :]
+    highest_deg.sort(order = 'annk')
+    highest_deg = highest_deg[::-1]
+
+    highest_annk = load_net_deg(annk_filename)[-annk_count :]
+
+    output_file = open(output_filename, "w+")
+    for line in highest_annk:
+        write_line(output_file, line)
+    for line in highest_deg:
+        write_line(output_file, line)
+    output_file.close()
 
 #-------------------------------------------------------------------------------
-# Script
+# Scripts
 #-------------------------------------------------------------------------------
 
-highest_deg = load_net_deg(deg_filename)[: -10000 + deg_count]
-highest_annk = load_net_deg(annk_filename)[-10000 + deg_count :]
-
-for line in highest_deg:
-    print_line(line)
-
-
-#output_file = open(out_filename, "w+")
-#for line in highest_deg:
-#    write_line(output_file, line)
-#output_file.close()
-
-'''
-highest_deg.sort(order = "annk")
-for line in highest_deg:
-    print_line(line)
-print("---")
-
-highest_annd = load_net_deg("annk")#[10000 - deg_count]
-for line in highest_annd:
-    print_line(line)
-'''
-
-#output = open(output_filename, "w+")
-#output = open(output_filename, "a")
-#output.close()
-
+make_decreasing_annk_list("60_annk.txt", 30, 30)
+make_decreasing_annk_list("80_annk.txt", 40, 40)
+make_decreasing_annk_list("100_annk.txt", 50, 50)
