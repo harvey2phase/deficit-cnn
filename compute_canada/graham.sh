747d14dc018e36c0a37539e8d8710c6ea948ffbe
Harvey Wang <harveyw@gra-login3.graham.sharcnet>
2019-07-16 19:15:09 -0400

    graham script might be ready; added graham tensorflow virtual env

--- compute_canada/graham.sh
+++ compute_canada/graham.sh
@@ -4,13 +4,6 @@
 #SBATCH --mem=1G
 #SBATCH --time=0-03:00
 
+module load cuda cudnn
 module load python/3.6
 source tensorflow/bin/activate
-
-module list
-sbatch NAME_OF_SCRIPT # runs when has resources
-
-# Create a virtual environment
-
-mkdir DIR
-virtualenv DIR
