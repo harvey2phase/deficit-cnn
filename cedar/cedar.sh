7e32f58150bb399fc6c303eb1dbf054e38717873
Harvey Wang <harveyw@gra-login3.graham.sharcnet>
2019-07-16 21:37:59 -0400

    reorganised data

---
+++ cedar/cedar.sh
@@ -0,0 +1,8 @@
+seff #find mem utilized
+module load python/3.6
+salloc --time=15 # gives interactive session
+nvidia-smi
+module list
+sbatch NAME_OF_SCRIPT # runs when has resources
+mkdir DIR
+virtualenv DIR
