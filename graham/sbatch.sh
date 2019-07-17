f1b4b016e9f0c3e5f5dde8f19b59bc5fe8fb1723
Harvey Wang <harveyw@gra-login1.graham.sharcnet>
2019-07-17 09:54:08 -0400

    added email func to sbatch

--- graham/sbatch.sh
+++ graham/sbatch.sh
@@ -5,6 +5,9 @@
 #SBATCH --cpus-per-task=12
 #SBATCH --time=0-03:00
 
+#SBATCH --mail-user=harvey@dal.ca
+#SBATCH --mail-type=ALL
+
 module load cuda cudnn
 module load python/3.6
 source tensorflow/bin/activate
