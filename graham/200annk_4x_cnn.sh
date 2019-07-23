a94ffd8bb49f13d5fffc1115e97acdf7bd1dd34d
Harvey Wang <harvey@dal.ca>
2019-07-23 13:40:21 -0400

    created sbatch script for 200annk cnn

---
+++ graham/200annk_4x_cnn.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --gres=gpu:1
+#SBATCH --mem=2500M
+#SBATCH --cpus-per-task=6
+#SBATCH --time=0-20:00
+
+#SBATCH --mail-user=harvey@dal.ca
+#SBATCH --mail-type=ALL
+
+module load cuda cudnn
+module load python/3.6
+source tensorflow/bin/activate
+python3 ../cnn/200annk_4x_cnn.sh
