551ba60d00eb9f6c3a13cc78045daf5acb8e46e8
Harvey Wang <harvey@dal.ca>
2019-07-30 14:38:11 -0300

    added e4 100 and 110 data

--- raw_data/model_data/split_data.py
+++ raw_data/model_data/split_data.py
@@ -11,12 +11,47 @@ gamma_0 = 0.00183       # (unit: per year) divide to get year.
 # File directories setup
 #-------------------------------------------------------------------------------
 
-output_folder = "e5_all/"
-death_year_filename = "e5_death_year.txt"
+output_folder = "e4_all/"
+death_year_filename = "e4_death_year.txt"
 raw_deficits_filename = "../../spencer_code/Data/"
-raw_deficits_filename += "RawDeficitsID1Gammap7.5Gamman6.5N10000Number100000R3."
+raw_deficits_filename += "RawDeficitsID1Gammap7.5Gamman6.5N10000Number10000R3."
 raw_deficits_filename += "0nd2Alpha2.27AvgDeg4ANDSingleSingleSeed1ScaleFreepA0"
 raw_deficits_filename += ".0"
+
+#-------------------------------------------------------------------------------
+# main
+#-------------------------------------------------------------------------------
+
+def main():
+    death_year = np.loadtxt(death_year_filename)
+    raw_deficits = np.loadtxt(raw_deficits_filename)
+
+    # The three columns in file "raw_deficits"
+    change_time = raw_deficits[:, 0] / gamma_0
+    node_id = raw_deficits[:, 1]
+    state = raw_deficits[:, 2]
+
+    raw_length = len(change_time) - 1
+
+
+    year = 0
+    f = create_textfile(death_year[year])
+
+    for change in range(0, raw_length - 1):
+        f.write(
+            str(change_time[change]) + "\t" +
+            str(int(node_id[change])) + "\t"
+        )
+        f.write(str(int(state[change])) + "\n")
+        if change_time[change + 1] - change_time[change] < 0:
+            year += 1
+            f.close()
+            f = create_textfile(death_year[year])
+
+    f.write(str(change_time[raw_length]) + "\t")
+    f.write(str(int(node_id[raw_length])))
+    f.write("\t" + str(int(state[raw_length])) + "\n")
+    f.close()
 
 #-------------------------------------------------------------------------------
 # Supporting functions
@@ -35,33 +70,4 @@ def create_textfile(filename):
 
     return f
 
-#-------------------------------------------------------------------------------
-# Scripts to split raw deficits
-#-------------------------------------------------------------------------------
-
-death_year = np.loadtxt(death_year_filename)
-raw_deficits = np.loadtxt(raw_deficits_filename)
-
-# The three columns in file "raw_deficits"
-change_time = raw_deficits[:, 0] / gamma_0
-node_id = raw_deficits[:, 1]
-state = raw_deficits[:, 2]
-
-raw_length = len(change_time) - 1
-
-
-year = 0
-f = create_textfile(death_year[year])
-
-for change in range(0, raw_length - 1):
-    f.write(str(change_time[change]) + "\t" + str(int(node_id[change])) + "\t")
-    f.write(str(int(state[change])) + "\n")
-    if change_time[change + 1] - change_time[change] < 0:
-        year += 1
-        f.close()
-        f = create_textfile(death_year[year])
-
-f.write(str(change_time[raw_length]) + "\t")
-f.write(str(int(node_id[raw_length])))
-f.write("\t" + str(int(state[raw_length])) + "\n")
-f.close()
+main()
