9c857eccd741f15a9ba90d42cd5d4cfa6b2a03ae
Harvey Wang <harvey@dal.ca>
2019-08-01 21:26:06 -0400

    created test2

---
+++ graham/test2/2x_cnn.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --gres=gpu:1
+#SBATCH --mem=2500M
+#SBATCH --cpus-per-task=6
+#SBATCH --time=0-40:00
+
+#SBATCH --mail-user=harvey@dal.ca
+#SBATCH --mail-type=ALL
+
+module load cuda cudnn
+module load python/3.6
+source tensorflow/bin/activate
+python3 ../../cnn/test2/2x_cnn.py
