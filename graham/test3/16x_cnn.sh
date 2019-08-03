fa5daa6ce3a5c09ae72a2c10a43847f1802097c1
Harvey Wang <harvey@dal.ca>
2019-08-03 12:48:03 -0400

    submitted test3

---
+++ graham/test3/16x_cnn.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --gres=gpu:1
+#SBATCH --mem=3G
+#SBATCH --cpus-per-task=6
+#SBATCH --time=0-30:00
+
+#SBATCH --mail-user=harvey@dal.ca
+#SBATCH --mail-type=ALL
+
+module load cuda cudnn
+module load python/3.6
+source ../tensorflow/bin/activate
+python3 ../../cnn/test3/16x_cnn.py
